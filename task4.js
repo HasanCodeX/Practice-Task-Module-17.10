655fce3c26561a8793558e31f37404dadc85e26a
HasanCodeX <dev.hasan10@gmail.com>
2025-02-08 21:41:39 +0600

    task 4

---
+++ task4.js
@@ -0,0 +1,5 @@
+var a1 = isNaN('11');  // Checking if '11' is NaN
+console.log(a1);       // Output: false
+
+var a2 = isNaN(2 - 10); // Checking if (2 - 10) is NaN
+console.log(a2);       // Output: false
